033d20376ce19d20786f71a8a49cb27d189099a0
Brett <24304042+Brallen@users.noreply.github.com>
2025-12-01 17:45:24 -0800

    Cleaning up answer some

--- 2025/app/Days/One.hs
+++ 2025/app/Days/One.hs
@@ -5,14 +5,14 @@ dayOne = do
   contents <- readFile "./app/Days/one-input.txt"
   let actions = lines contents
       result = foldl' calculation (50, 0) actions
-  putStrLn . show $ snd result
+  print $ snd result
 
 calculation :: (Int, Int) -> String -> (Int, Int)
 calculation (position, numZeros) action = do
   let (direction : amount) = action
       potentialPosition = case direction of
-        'L' -> (position - (read @Int amount))
-        'R' -> (position + (read @Int amount))
+        'L' -> position - read @Int amount
+        'R' -> position + read @Int amount
         _ -> position
       newPosition = mod potentialPosition 100
       newZeros = case newPosition == 0 of
